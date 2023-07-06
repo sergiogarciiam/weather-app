c1a7e81ad4aa94d204747d08260b912793ab6312
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-07-06 13:23:25 +0200

    Initial setup

---
+++ index.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const test = document.createElement("h1");
+test.textContent = "hello world";
+document.body.appendChild(test);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdGVzdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJoMVwiKTtcbnRlc3QudGV4dENvbnRlbnQgPSBcImhlbGxvIHdvcmxkXCI7XG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKHRlc3QpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9

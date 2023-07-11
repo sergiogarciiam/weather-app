15fa68ecf86c3a8a2b362e224e1b57e6a4fb56d8
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-07-11 10:30:46 +0200

    Add links

--- src/components/links.js
+++ src/components/links.js
@@ -1,14 +1,23 @@
 export const createLinks = () => {
   const links = document.createElement("p");
   const author = document.createElement("a");
+  const separator = document.createElement("p");
   const source = document.createElement("a");
 
   links.classList.add("links");
+
   author.textContent = "Sergio García";
+  author.href = "https://github.com/sergiogarciiam";
+  author.target = "__blank";
+
+  separator.textContent = " | ";
+
   source.textContent = "Source Code";
+  source.href = "https://github.com/sergiogarciiam/weather-app";
+  source.target = "__blank";
 
   links.appendChild(author);
-  links.innerText += " | ";
+  links.appendChild(separator);
   links.appendChild(source);
 
   return links;
